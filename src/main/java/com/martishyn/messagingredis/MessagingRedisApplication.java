dcb23117cd3cd91f769bd228412c87e6597065cc
Andrii Martishyn <contactgently@gmail.com>
2024-05-02 12:19:16 +0300

    Initial commit

---
+++ src/main/java/com/martishyn/messagingredis/MessagingRedisApplication.java
@@ -0,0 +1,13 @@
+package com.martishyn.messagingredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingRedisApplication.class, args);
+    }
+
+}
